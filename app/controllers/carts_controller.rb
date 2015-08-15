92a31f70baf787870a9962afa99847e3a73b8b89
Javier Rodriguez <javierorodriguez@gmail.com>
2015-08-16 01:04:28 +0300

    handle error handling

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,8 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  
+  # Rescuing the exception raised by 'Cart.find()', call to 'invalid_cart'
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
   # GET /carts
   # GET /carts.json
@@ -70,5 +73,12 @@ class CartsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
       params[:cart]
+    end
+
+    def invalid_cart
+      # We log the error using the logger attribute that every controller has.
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      # And redirecting to the store url with a notice flash.
+      redirect_to store_url, notice: 'Invalid cart'
     end
 end
